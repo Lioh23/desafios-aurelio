3f23fc5b39922080992d59d7d1b6d56e7552ee07
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-24 14:59:14 -0300

    feat: implementa o jogo de adivinhar completo

--- tarefas/05.jogo_adivinhe_um_numero_html/app.js
+++ tarefas/05.jogo_adivinhe_um_numero_html/app.js
@@ -1,2 +1,33 @@
-const inputNumero = document.getElementById('numero');
-const btnJogar = document.getElementById('btn-jogar');
+let inputNumero = document.getElementById('numero');
+let btnCalcular = document.getElementById('btn-jogar');
+
+let palpiteMin = 1;
+let palpiteMax = 10;
+
+let numeroAleatorio = gerarNumeroAleatorio(palpiteMin, palpiteMax);
+
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function palpitar() {
+    if (isNaN(inputNumero.value) || inputNumero.value == '' || inputNumero.value < palpiteMin || inputNumero.value > palpiteMax) {
+        alert('O número inserido não é válido!')
+        return;
+    }
+
+    let palpite = inputNumero.value;
+
+    if (palpite == numeroAleatorio) {
+        alert('Parabéns, você acertou!');
+        reiniciarJogo();
+    } else {
+        alert(`Que pena, você errou! O número sorteado foi ${numeroAleatorio}`);
+        reiniciarJogo();
+    }
+}
+
+function reiniciarJogo() {
+    inputNumero.value = '';
+    return numeroAleatorio = gerarNumeroAleatorio(palpiteMin, palpiteMax);    
+}
