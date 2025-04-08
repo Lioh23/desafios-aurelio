fe3df91c3142b78e505dec9fd55fc3a70f9f52cd
Lucas Monteiro <lmontecosta@gmail.com>
2025-04-08 15:52:21 -0300

    feat: implementa a alteração de cor

--- tarefas/06.desafio_rgb/app.js
+++ tarefas/06.desafio_rgb/app.js
@@ -1,12 +1,49 @@
-let red = document.getElementById('range-r').value;
-let green = document.getElementById('range-g').value;
-let blue = document.getElementById('range-b').value;
+
+let redLevel = document.getElementById('range-r');
+let greenLevel = document.getElementById('range-g');
+let blueLevel = document.getElementById('range-b');
+
+let redLabel = document.getElementById('label-r');
+let greenLabel = document.getElementById('label-g');
+let blueLabel = document.getElementById('label-b');
+
 let square = document.getElementById('square');
 
-function mudarCor(r, g, b) {
-    square.style = `background-color:rgb(${r}, ${g}, ${b});`
+let rgb = {
+    red: 0,
+    green: 0,
+    blue: 0
 }
 
-mudarCor(red, blue, green);
+square.style = `background-color:rgb(0, 0, 0);`
 
-console.log(square);
+
+function mudarCor() {
+    for (let cor in rgb) {
+        document.getElementById('square').style = `background-color:rgb(${rgb.red}, ${rgb.green}, ${rgb.blue});`
+    }
+}
+
+//define a cor padrão do quadrado
+mudarCor();
+
+//evento para alterar o vermelho
+redLevel.addEventListener("mousemove", (event) => {
+    redLabel.innerHTML = `${event.target.value}`;
+    rgb.red = `${event.target.value}`;
+    mudarCor();
+});
+
+//evento para alterar o verde
+greenLevel.addEventListener("mousemove", (event) => {
+    greenLabel.innerHTML = `${event.target.value}`;
+    rgb.green = `${event.target.value}`;
+    mudarCor();
+});
+
+//evento para alterar o azul
+blueLevel.addEventListener("mousemove", (event) => {
+    blueLabel.innerHTML = `${event.target.value}`;
+    rgb.blue = `${event.target.value}`
+    mudarCor();
+});
